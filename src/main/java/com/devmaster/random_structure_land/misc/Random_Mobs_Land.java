2609b2369349d2baacf071a2339c323577b46cc5
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2023-09-06 23:31:05 -0400

    initial set up
    lets go randomize mob spawns

---
+++ src/main/java/com/devmaster/random_structure_land/misc/Random_Mobs_Land.java
@@ -0,0 +1,36 @@
+package com.devmaster.random_structure_land.misc;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("random_mobs_land")
+public class Random_Mobs_Land {
+    public static ServerWorld serverWorldCache;
+
+    public static final Logger LOGGER = LogManager.getLogger("Random Mobs Land");
+    public static final String MOD_ID = "random_mobs_land";
+
+    public Random_Mobs_Land() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
+
+
+
